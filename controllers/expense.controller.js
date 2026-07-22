abe80e451c543e09fdde36f6e54211f2e659ff58
Aysha2812 <tbkalvin.2812@gmail.com>
2026-07-22 16:20:52 +0300

    fimished code

--- controllers/expense.controller.js
+++ controllers/expense.controller.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 
-const Expense=require("../models/expense");
+const Expense=require("../models/Expense");
 const Category=require("../models/category");
 
 //GET-New expense form
